96c15a895a38b9013948a846f00dad47978a1d3f
y0c <holnet1026@gmail.com>
2020-11-14 19:38:37 +0900

    chore: add user ddl script

--- db/init/user.sql
+++ db/init/user.sql
@@ -1,12 +1,14 @@
-CREATE TABLE nodelab.user
-(
-    `email`          varchar(40)     NOT NULL    COMMENT '이메일',
-    `username`       varchar(20)     NOT NULL    COMMENT '유저이름',
-    `password`       varchar(200)    NULL        COMMENT '비밀번호',
-    `profile_image`  integer         NULL        COMMENT '프로필이미지',
-    `intro`          text            NULL        COMMENT '소개',
-    `github_url`     varchar(40)     NULL        COMMENT 'Github 주소',
-    PRIMARY KEY (email)
-);
-
-ALTER TABLE nodelab.user COMMENT '회원';
+CREATE TABLE nodelab.user (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `email` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '이메일',
+    `username` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '유저닉네임',
+    `password` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '패스워드',
+    `profile_image` int DEFAULT NULL COMMENT '프로필 이미지',
+    `intro` text COLLATE utf8mb4_unicode_ci COMMENT '소개글',
+    `github_url` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Github URL',
+    `created_at` datetime DEFAULT NULL,
+    `updated_at` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `email_UNIQUE` (`email`),
+    UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='회원';
