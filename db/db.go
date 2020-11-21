706b165d99a45e784dcf750b676c1982ab716d9e
y0c <holnet1026@gmail.com>
2020-11-21 23:24:51 +0900

    refactor: apply db connection pool

--- db/db.go
+++ db/db.go
@@ -5,11 +5,22 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	c "github.com/no-de-lab/nodelab-server/config"
+	"time"
+)
+
+const (
+	maxIdleConns = 10
+	maxOpenConns = 10
+	maxConnLife  = 5 * time.Minute
 )
 
 func NewDatabase(config *c.Configuration) *sqlx.DB {
 	connectionStr := fmt.Sprintf("%s:%s@(%s:3306)/%s", config.Database.Username, config.Database.Password, config.Database.Host, config.Database.Database)
 	db := sqlx.MustConnect("mysql", connectionStr)
+
+	db.SetMaxIdleConns(maxIdleConns)
+	db.SetMaxOpenConns(maxOpenConns)
+	db.SetConnMaxLifetime(maxConnLife)
 
 	return db
 }
