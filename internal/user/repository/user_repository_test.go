0e4491945177f839e5871c262f87bb316334bc1a
Jeff Kim <eunkeekim@gmail.com>
2020-11-21 21:37:23 -0500

    fix: change column name to profile_image_id

--- internal/user/repository/user_repository_test.go
+++ internal/user/repository/user_repository_test.go
@@ -2,12 +2,13 @@ package repository_test
 
 import (
 	"context"
+	"testing"
+	"time"
+
 	"github.com/jmoiron/sqlx"
 	"github.com/no-de-lab/nodelab-server/internal/user/repository"
 	"github.com/stretchr/testify/assert"
 	sqlmock "gopkg.in/DATA-DOG/go-sqlmock.v1"
-	"testing"
-	"time"
 )
 
 const timeFormat = "2006-01-02 15:04:05"
@@ -18,7 +19,7 @@ func TestFindById(t *testing.T) {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
 
-	rows := sqlmock.NewRows([]string{"id", "email", "username", "intro", "profile_image", "created_at", "updated_at"}).
+	rows := sqlmock.NewRows([]string{"id", "email", "username", "intro", "profile_image_id", "created_at", "updated_at"}).
 		AddRow(1, "test@gmail.com", "test", "test intro", 0, time.Now().Format(timeFormat), time.Now().Format(timeFormat))
 
 	mock.ExpectQuery("SELECT id").WillReturnRows(rows)
