7939d3a89c4aa5e8daf148620ff0f83b16a89ec8
y0c <holnet1026@gmail.com>
2021-01-17 17:24:33 +0900

    feat: apply graphql endpoint

--- cmd/nodelabd/main.go
+++ cmd/nodelabd/main.go
@@ -3,26 +3,31 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/http"
+	"time"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/no-de-lab/nodelab-server/api"
+	"github.com/no-de-lab/nodelab-server/graphql"
 	"github.com/no-de-lab/nodelab-server/internal/logger"
 	log "github.com/sirupsen/logrus"
 	"github.com/tylerb/graceful"
-	"net/http"
-	"time"
 )
 
 func main() {
 	flag.Parse()
 
 	container := InitializeDIContainer()
+	rootResolver := InitializeResolver()
 	logger.InitLogging()
 
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.HTTPErrorHandler = api.ErrorHandler
+
+	graphql.SetupGraphQL(e, rootResolver)
 
 	for _, h := range container.Handlers {
 		h.SetupRoutes(e)
