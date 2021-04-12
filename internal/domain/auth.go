30b0800ca648dad50d62455c9b0ebeed05dec0cc
Jeff Kim <eunkeekim@gmail.com>
2021-04-12 01:44:44 -0400

    feat: Define auth model and domain

--- internal/domain/auth.go
+++ internal/domain/auth.go
@@ -4,12 +4,29 @@ import (
 	"context"
 
 	am "github.com/no-de-lab/nodelab-server/internal/auth/model"
-	"github.com/no-de-lab/nodelab-server/internal/user/model"
+	"gopkg.in/guregu/null.v4"
 )
 
+type UserAccount struct {
+	ID          int64       `db:"id"`
+	Email       string      `db:"email"`
+	Password    null.String `db:"password"`
+	Provider    null.String `db:"provider"`
+	ProviderID  null.String `db:"provider_id"`
+	AccessToken null.String `db:"access_token"`
+	CreatedAt   string      `db:"created_at"`
+	UpdatedAt   string      `db:"updated_at"`
+}
+
+type AuthRepository interface {
+	FindAccountByEmail(ctx context.Context, email string) (*UserAccount, error)
+	CreateUserByEmail(ctx context.Context, user *UserAccount) error
+	CreateUserBySocial(ctx context.Context, user *UserAccount) error
+}
 type AuthService interface {
-	Login(ctx context.Context, form *am.LoginModel) (err error)
-	Signup(ctx context.Context, user *model.CreateUserModel) (err error)
+	Login(ctx context.Context, form *am.LoginModel) error
+	SignupEmail(ctx context.Context, user *am.SignupEmailModel) error
+	SignupSocial(ctx context.Context, user *am.SignupSocialModel) error
 	// TODO: add detail
-	SocialLogin() (err error)
+	SocialLogin() error
 }
