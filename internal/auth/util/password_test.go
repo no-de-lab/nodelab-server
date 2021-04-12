d2f76ee0a4f614a79b3b64b6fa095b493a3dca88
Jeff Kim <eunkeekim@gmail.com>
2021-04-12 01:45:40 -0400

    feat: Add bcrypt to encrypt password

---
+++ internal/auth/util/password_test.go
@@ -0,0 +1,22 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestPassword(t *testing.T) {
+	password := "secret"
+
+	hashedPassword, err := HashedPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword)
+
+	err = CheckPassword(password, hashedPassword)
+	require.NoError(t, err)
+
+	err = CheckPassword("haha", hashedPassword)
+	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+}
