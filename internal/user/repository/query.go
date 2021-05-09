52fccd3c68a4bdaf5f36f3e1c8e25b050f9af70f
Jeff Kim <eunkeekim@gmail.com>
2021-05-09 01:17:13 -0400

    style: Format queries

--- internal/user/repository/query.go
+++ internal/user/repository/query.go
@@ -1,9 +1,11 @@
 package repository
 
 // TODO: profile_image_id join 추가
-const FindByIDQuery = `SELECT id, email, username, intro, profile_image_id, created_at, updated_at FROM user WHERE id = ? `
-const FindByEmailQuery = `SELECT id, email, username, profile_image_id, intro, github_url, created_at, updated_at FROM user WHERE email = ?`
-const CreateUserQuery = `
+const (
+	FindByIDQuery    = `SELECT id, email, username, intro, profile_image_id, created_at, updated_at FROM user WHERE id = ? `
+	FindByEmailQuery = `SELECT id, email, username, profile_image_id, intro, github_url, created_at, updated_at FROM user WHERE email = ?`
+	CreateUserQuery  = `
 	INSERT INTO user(email, username, password, intro, profile_image_id, created_at, updated_at)
 	VALUES (:email, :username, :password, :intro, :profile_image_id, now(), now())
 `
+)
