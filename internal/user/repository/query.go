4ef00446b8b1c02efa115d9a13fedfae6157af24
Jeff Kim <eunkeekim@gmail.com>
2021-05-15 22:06:24 -0400

    feat: Add user repository for update user

--- internal/user/repository/query.go
+++ internal/user/repository/query.go
@@ -2,6 +2,10 @@ package repository
 
 // TODO: profile_image_id join 추가
 const (
-	FindByIDQuery    = `SELECT id, email, username, intro, profile_image_id, created_at, updated_at FROM user WHERE id = ? `
-	FindByEmailQuery = `SELECT id, email, username, profile_image_id, intro, github_url, created_at, updated_at FROM user WHERE email = ?`
+	FindByIDQuery          = `SELECT id, email, username, intro, profile_image_id, created_at, updated_at FROM user WHERE id = ? `
+	FindByEmailQuery       = `SELECT id, email, username, profile_image_id, intro, github_url, created_at, updated_at FROM user WHERE email = ?`
+	updateUserByEmailQuery = `
+		UPDATE user
+		SET username=:username, intro=:intro, position=:position, interest=:interest, github_url=:github_url, updated_at=NOW()
+		WHERE email=:email;`
 )
