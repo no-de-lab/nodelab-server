e02bfbc42fa31bff592246fe96fc471f84a1e547
kyu <jqyu.lee@gmail.com>
2021-05-18 23:11:54 +0900

    fix: get column provider_id, provider in findAccountByEmailQuery

--- internal/auth/repository/query.go
+++ internal/auth/repository/query.go
@@ -2,7 +2,7 @@ package repository
 
 // Query for User Domain
 const (
-	findAccountByEmailQuery = `SELECT email, password FROM account WHERE email = ?`
+	findAccountByEmailQuery = `SELECT email, password, provider_id, provider FROM account WHERE email = ?`
 	createUserByEmailQuery  = `
 	INSERT INTO account(email, password, created_at, updated_at)
 	VALUES (:email, :password, now(), now())`
