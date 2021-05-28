997178417d0bc10d9ccf89bb1132a6a5fbdea3b4
David Hwang <69114096+davidhwang-ij@users.noreply.github.com>
2021-05-28 00:47:18 -0400

    Added CORS middleware (#66)
    * Added CORS middleware
    
    * Added wildcard to AllowOrigins and a comment for the CORS function

--- internal/auth/delivery/middleware/middleware.go
+++ internal/auth/delivery/middleware/middleware.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/no-de-lab/nodelab-server/internal/auth/util"
 )
 
@@ -49,5 +50,16 @@ func Authorize(jwtMaker util.JWTMaker) echo.MiddlewareFunc {
 			c.Set(UserPayloadCtxKey, payload)
 			return next(c)
 		}
+	}
+}
+
+// CORS allows us to customize the CORS headers
+func CORS(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		CORSMiddleware := middleware.CORSWithConfig(middleware.CORSConfig{
+			AllowOrigins: []string{"*"},
+		})
+		CORSHandler := CORSMiddleware(next)
+		return CORSHandler(c)
 	}
 }
