c0413be4aa37372d1349fbd2c535cafdf2329109
kyu <jqyu.lee@gmail.com>
2021-06-10 16:54:35 +0900

    feat: add study repository

---
+++ internal/study/repository/study_repository.go
@@ -0,0 +1,52 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/no-de-lab/nodelab-server/internal/domain"
+)
+
+type studyDBRepository struct {
+	DB *sqlx.DB
+}
+
+// NewStudyRepository creates a new study repository
+func NewStudyRepository(db *sqlx.DB) domain.StudyRepository {
+	return &studyDBRepository{
+		db,
+	}
+}
+
+// FindByID finds a study by ID
+func (r *studyDBRepository) FindByID(ctx context.Context, id int) (study *domain.Study, err error) {
+	s := domain.Study{}
+	err = r.DB.GetContext(ctx, &s, FindByIDQuery, id)
+
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, nil
+	}
+
+	if err != nil {
+		return
+	}
+
+	return &s, nil
+}
+
+func (r *studyDBRepository) FindByTitle(ctx context.Context, title string) (study *[]domain.Study, err error) {
+	s := []domain.Study{}
+	err = r.DB.GetContext(ctx, &s, FindByTitleQuery, title)
+
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, nil
+	}
+
+	if err != nil {
+		return
+	}
+
+	return &s, nil
+}
