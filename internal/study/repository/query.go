8cfa809927ad6e0eb4cd6487437f83be54f9ba4a
kyu <jqyu.lee@gmail.com>
2021-07-15 07:02:57 +0900

    feat: make create update delete repository

--- internal/study/repository/query.go
+++ internal/study/repository/query.go
@@ -3,11 +3,15 @@ package repository
 // Study query
 const (
 	// FindByIDQuery study leader profile image 파일 테이블 따로 조인 필요
-	FindByIDQuery    = `SELECT id, studyname, created_at, updated_at, 'limit', start_date, finish_date, summary, title, content, notice, leader_id FROM study WHERE status = "OPEN" AND id = ?;`
+	FindByIDQuery    = "SELECT id, `name`, created_at, updated_at, `limit`, start_date, finish_date, summary, title, content, notice, leader_id, `status`, thumbnail_url FROM study WHERE id = ? and deleted_at is null;"
 	FindByTitleQuery = `
-	SELECT id, studyname, created_at, updated_at, 'limit', start_date, finish_date, summary, title, content, notice, leader_id
-	FROM study
-	WHERE title like %?%
-	AND status = 'OPEN';
-	`
+		SELECT id, 'name', created_at, updated_at, 'limit', start_date, finish_date, summary, title, content, notice, leader_id
+		FROM study
+		WHERE title like %?%
+		AND status = 'OPEN'
+		AND deleted_at is null;`
+	CreateStudyQuery = "INSERT INTO study(`name`, `limit`, start_date, finish_date, summary, title, content, leader_id, notice, thumbnail_url, `status`) VALUES (:name, :limit, :start_date, :finish_date, :summary, :title, :content, :leader_id, :notice, :thumbnail, 'PROGRESS');"
+	UpdateStudyQuery = "UPDATE study SET name=coalesce(:name, `name`), `limit`=coalesce(:limit, `limit`), start_date=coalesce(:start_date, start_date), finish_date=coalesce(:finish_date, finish_date), summary=coalesce(:summary, summary), title=coalesce(:title, title), content=coalesce(:content, content), notice=coalesce(:notice, notice), thumbnail_url=coalesce(:thumbnail_url, thumbnail_url), `status`=coalesce(:status, status) WHERE id=:id AND deleted_at is null;"
+	DeleteStudyQuery = `DELETE FROM study where id=?;`
+	FindByEmailQuery = `SELECT id, email, username, profile_image_id, intro, github_url, created_at, updated_at FROM user WHERE email = ?`
 )
--- internal/study/repository/study_repository.go
+++ internal/study/repository/study_repository.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/no-de-lab/nodelab-server/internal/domain"
+	sm "github.com/no-de-lab/nodelab-server/internal/study/model"
 )
 
 type studyDBRepository struct {
@@ -21,9 +23,25 @@ func NewStudyRepository(db *sqlx.DB) domain.StudyRepository {
 }
 
 // FindByID finds a study by ID
-func (r *studyDBRepository) FindByID(ctx context.Context, id int) (study *domain.Study, err error) {
+func (r *studyDBRepository) FindByID(ctx context.Context, id int) (*domain.Study, error) {
 	s := domain.Study{}
-	err = r.DB.GetContext(ctx, &s, FindByIDQuery, id)
+	err := r.DB.GetContext(ctx, &s, FindByIDQuery, id)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, err
+	}
+
+	// !TODO : scan error on column 'limit' converting type []uint8 to int error
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	return &s, nil
+}
+
+// FindByTitle find studies by title
+func (r *studyDBRepository) FindByTitle(ctx context.Context, title string) (*[]domain.Study, error) {
+	s := []domain.Study{}
+	err := r.DB.GetContext(ctx, &s, FindByTitleQuery, title)
 
 	if errors.Is(err, sql.ErrNoRows) {
 		return nil, nil
@@ -36,9 +54,48 @@ func (r *studyDBRepository) FindByID(ctx context.Context, id int) (study *domain
 	return &s, nil
 }
 
-func (r *studyDBRepository) FindByTitle(ctx context.Context, title string) (study *[]domain.Study, err error) {
-	s := []domain.Study{}
-	err = r.DB.GetContext(ctx, &s, FindByTitleQuery, title)
+// CreateStudy create study
+func (r *studyDBRepository) CreateStudy(ctx context.Context, input *sm.CreateStudy) (*domain.Study, error) {
+	fmt.Println(input.Notice)
+	res, err := r.DB.NamedExecContext(ctx, CreateStudyQuery, input)
+	if err != nil {
+		return nil, err
+	}
+
+	studyID, err := res.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(studyID)
+
+	return r.FindByID(ctx, int(studyID))
+}
+
+// UpdateStudy update study
+func (r *studyDBRepository) UpdateStudy(ctx context.Context, input *sm.UpdateStudy) (*domain.Study, error) {
+	_, err := r.DB.NamedExecContext(ctx, UpdateStudyQuery, input)
+	if err != nil {
+		return nil, err
+	}
+
+	return r.FindByID(ctx, input.ID)
+}
+
+// DeleteStudy delete study
+func (r *studyDBRepository) DeleteStudy(ctx context.Context, id int) (flag bool, err error) {
+	_, err = r.DB.ExecContext(ctx, DeleteStudyQuery, id)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+// FindByEmail finds a user by email
+func (r *studyDBRepository) FindByEmail(ctx context.Context, email string) (*domain.User, error) {
+	u := domain.User{}
+	err := r.DB.GetContext(ctx, &u, FindByEmailQuery, email)
 
 	if errors.Is(err, sql.ErrNoRows) {
 		return nil, nil
@@ -48,5 +105,5 @@ func (r *studyDBRepository) FindByTitle(ctx context.Context, title string) (stud
 		return nil, err
 	}
 
-	return &s, nil
+	return &u, nil
 }
