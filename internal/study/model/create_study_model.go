6d5f1e8c9f979d5238fab4989fcb378ec68bfd83
kyu <jqyu.lee@gmail.com>
2021-07-15 07:02:10 +0900

    feat: define study create, update model

---
+++ internal/study/model/create_study_model.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	"time"
+)
+
+// CreateStudy is used as a model for create a study
+type CreateStudy struct {
+	Name       string    `json:"name" db:"name"`
+	Limit      int       `json:"limit" db:"limit"`
+	StartDate  time.Time `json:"start_date" db:"start_date"`
+	FinishDate time.Time `json:"finish_date" db:"finish_date"`
+	Summary    string    `json:"summary" db:"summary"`
+	Title      string    `json:"title" db:"title"`
+	Content    string    `json:"content" db:"content"`
+	LeaderID   int       `json:"leader_id" db:"leader_id"`
+	Notice     string    `json:"notice" db:"notice"`
+	Thumbnail  string    `json:"thumbnail" db:"thumbnail"`
+}
---
+++ internal/study/model/update_study_model.go
@@ -0,0 +1,22 @@
+package model
+
+import (
+	"time"
+
+	gqlschema "github.com/no-de-lab/nodelab-server/graphql/generated"
+)
+
+// UpdateStudy is used as a model for create a study
+type UpdateStudy struct {
+	ID         int                    `json:"id" db:"id"`
+	Name       *string                `json:"name" db:"name"`
+	Limit      *int                   `json:"limit" db:"limit"`
+	StartDate  *time.Time             `json:"start_date" db:"start_date"`
+	FinishDate *time.Time             `json:"finish_date" db:"finish_date"`
+	Summary    *string                `json:"summary" db:"summary"`
+	Title      *string                `json:"title" db:"title"`
+	Content    *string                `json:"content" db:"content"`
+	Notice     *string                `json:"notice" db:"notice"`
+	Thumbnail  *string                `json:"thumbnail" db:"thumbnail_url"`
+	Status     *gqlschema.StudyStatus `json:"status" db:"status"`
+}
