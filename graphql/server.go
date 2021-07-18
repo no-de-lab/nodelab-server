630ad1f9e51d38ca675ed8f3c7773a05275530d4
kyu <jqyu.lee@gmail.com>
2021-07-18 12:57:45 +0900

    fix: comment others

--- graphql/server.go
+++ graphql/server.go
@@ -29,9 +29,9 @@ func SetupGraphQL(e *echo.Echo, resolver *resolver.Resolver, cfg *config.Configu
 	playgroundHandler := playground.Handler("GraphQL", graphqlEndpoint)
 
 	e.Use(middleware.Authorize(util.NewJWTMaker(cfg)))
-	e.Use(EchoContextToContextMiddleware)
+	// e.Use(EchoContextToContextMiddleware)
 	// e.Use(CORSMiddleware)
-	e.Use(CORSMiddlewareWrapper)
+	// e.Use(CORSMiddlewareWrapper)
 
 	e.OPTIONS(graphqlEndpoint, addHeaders)
 	e.POST(graphqlEndpoint, func(c echo.Context) error {
@@ -43,7 +43,7 @@ func SetupGraphQL(e *echo.Echo, resolver *resolver.Resolver, cfg *config.Configu
 		playgroundHandler.ServeHTTP(c.Response(), c.Request())
 		return nil
 	})
-	e.OPTIONS(graphqlEndpoint, echo.MethodNotAllowedHandler)
+	// e.OPTIONS(graphqlEndpoint, echo.MethodNotAllowedHandler)
 }
 
 func addHeaders(c echo.Context) error {
